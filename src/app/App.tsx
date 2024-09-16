3ee3812fad8efa0ce30edc1eafad9c18665c19d8
arantzaLozada <yeseniaarantza@gmail.com>
2024-09-16 00:10:48 -0500

    Features: add new project

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'antd';
+
+export const App = () => {
+  return (
+    <div className="content">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
